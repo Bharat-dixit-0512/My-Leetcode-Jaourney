7d19f2d62dcdc6595b5d41f3521f38609306bd8e
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-01-19 20:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (51.33%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -4,8 +4,7 @@ class Solution {
         for(int i=n-1;i>=0;i--){
             if(digits[i]==9){
                 digits[i]=0;
-            }
-            else{
+            }else{
                 digits[i]=digits[i]+1;
                 return digits;
             }
