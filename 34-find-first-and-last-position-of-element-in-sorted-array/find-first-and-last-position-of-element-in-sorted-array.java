c7077b203a2fd77be9acc73ec77554e008a01eb4
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-01-20 13:50:11 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (59.72%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,29 +1,42 @@
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
         int result[]=new int[2];
-        if(nums.length==0){
-            result[0]=-1;
-            result[1]=-1;
-            return result;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                result[0]=i;
-                break;
-            }
-            else{
-                result[0]=-1;
-            }
-        }
-        for(int i=nums.length-1;i>=0;i--){
-            if(nums[i]==target){
-                result[1]=i;
-                break;
-            }
-            else{
-                result[1]=-1;
-            }
-        }
+        result[0]=first(nums,target);
+        result[1]=last(nums,target);
         return result;
+    }
+    public int first(int[] nums,int target){
+        int left=0,right=nums.length-1,idx=-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                right=mid-1;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+            if(nums[mid]==target) idx=mid;
+        }
+        return idx;
+    }
+    public int last(int[] nums,int target){
+        int left=0,right=nums.length-1,idx=-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                left=mid+1;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+            if(nums[mid]==target) idx=mid;
+        }
+        return idx;
     }
 }
