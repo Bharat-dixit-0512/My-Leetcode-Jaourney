6480653c48efb801ce7dc3452fe84dbcb6b9c1be
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-01-23 21:18:59 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (97.63%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,15 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int idx = 0 ; idx < prices.length-1; idx++){
-            if( prices[idx] < prices[idx+1] ){
-                list.add( prices[idx+1]- prices[idx]);
-            }
-        }
-        int sum=0;
-        for(int i=0;i<list.size();i++){
-            sum=sum+list.get(i);
-        }
-        return sum;    
+      int second = 0, ans = 0;
+      for(int i = 0; i<prices.length;i++){
+            if(prices[i]>prices[second]) ans += prices[i]-prices[second];
+            second = i;
+      }  
+      return ans;
     }
 }
