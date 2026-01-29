a8068a5198639d87822ecf79982541c11da19050
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-01-29 19:27:30 +0530

    Time: 4 ms (79.84%) | Memory: 46.6 MB (21.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() !=t.length())return false;
-        HashMap<Character,Integer> hm1=new HashMap<>();
-        for(char ch:s.toCharArray()){
-            hm1.put(ch,hm1.getOrDefault(ch,0)+1);
-        }
-        for(char ch:t.toCharArray()){
-            hm1.put(ch,hm1.getOrDefault(ch,0)-1);
-
-        }
-        for(int c:hm1.values()){
-            if(c!=0){
-                return false;
-            }
-        }
-        return true;
+        char[] ch1=s.toCharArray();
+        char[] ch2=t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        return Arrays.equals(ch1,ch2);
     }
 }
