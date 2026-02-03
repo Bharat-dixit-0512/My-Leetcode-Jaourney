48a95ac046b5986830039aac362e1ea52248f403
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-02-03 13:23:46 +0530

    Time: 4 ms (51.36%) | Memory: 44.7 MB (53.55%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s=s.toLowerCase();
-        String ns="";
+        StringBuilder ns=new StringBuilder();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch>='a' && ch<='z' || ch>='0' && ch<='9'){
-                ns+=ch;
+                ns.append(ch);
             }
         }
         int start=0;
